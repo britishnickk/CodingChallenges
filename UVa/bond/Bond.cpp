87a7a437c2cc620e077f73631d7aaa6abc3322b3
Nick White <britishnickk@gmail.com>
2016-02-22 22:22:54 -0600

    initial commit for bond problem

---
+++ UVa/bond/Bond.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+int num_cities;
+int num_roads;
+cin>>num_cities;
+cin>>num_roads;
+
+cout<<num_cities<<endl;
+
+cout<<num_roads<<endl;
+}
---
+++ UVa/bond/test.sh
@@ -0,0 +1,3 @@
+cpp Bond.cpp -o Bond.out -lm -lcrypt -O2 -std=c++11 -pipe -DONLINE_JUDGE
+./Bond.out << test.txt
+
