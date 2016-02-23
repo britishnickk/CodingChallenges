ac2271d26fb7382bcff8a5cb9e065a62c3ec77ba
Nick White <britishnickk@gmail.com>
2016-02-22 22:28:04 -0600

    test script working properly

--- UVa/bond/test.sh
+++ UVa/bond/test.sh
@@ -1,3 +1,3 @@
-cpp Bond.cpp -o Bond.out -lm -lcrypt -O2 -std=c++11 -pipe -DONLINE_JUDGE
-./Bond.out << test.txt
+g++ Bond.cpp -o Bond.out -lm -lcrypt -O2 -std=c++11 -pipe -DONLINE_JUDGE
+cat test.txt | ./Bond.out 
 
