aec462283fba64c6fb0b462879e96e2b191ca438
Nick White <britishnickk@gmail.com>
2016-02-26 19:36:22 -0600

    Can read in roads

--- UVa/bond/Bond.cpp
+++ UVa/bond/Bond.cpp
@@ -1,13 +1,38 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 int main(){
 
-int num_cities;
-int num_roads;
-cin>>num_cities;
-cin>>num_roads;
+	int num_cities;
+	int num_roads;
+	int garbage;
+	cin>>num_cities;
+	cin>>num_roads;
+	//get rid of endl
+	cin>>garbage;
 
-cout<<num_cities<<endl;
+	//int roads[num_roads][3];
+	vector<int*> roads;
 
-cout<<num_roads<<endl;
+	for(int i=0; i<num_roads; i++){
+		int a;
+		int b;
+		int danger;
+		cin>>a;
+		cin>>b;
+		cin>>danger;
+		int* road = new int(3);
+		road[0]=a;
+		road[1]=b;
+		road[2]=danger;
+		roads.push_back(road);
+
+	}
+
+	for(int* road : roads){	//DO THIS LAST
+		cout<< road[0]<<" "<<road[1]<<" "<<road[2]<<endl;
+		delete[] road;
+
+	}
+
 }
